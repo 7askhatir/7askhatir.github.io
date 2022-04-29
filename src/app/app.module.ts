6ca7389e4822d30357c09c891dc4a752c106c067
7askhatir <askhatir7@gmail.com>
2022-04-29 07:30:42 +0000

    Init app

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServicesComponent } from './companant/services/services.component';
+import { HeaderComponent } from './companant/header/header.component';
+import { ContactComponent } from './companant/header/contact/contact.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SpecialiteComponent } from './companant/specialite/specialite.component';
+import { MoiComponent } from './companant/moi/moi.component';
+import { EducationComponent } from './companant/moi/education/education.component';
+import { SwiperModule } from 'swiper/angular';
+import { ExperComponent } from './companant/exper/exper.component';
+  
+  @NgModule({
+  declarations: [
+    AppComponent,
+    ServicesComponent,
+    HeaderComponent,
+    ContactComponent,
+    SpecialiteComponent,
+    MoiComponent,
+    EducationComponent,
+    ExperComponent  ],
+  imports: [
+    SwiperModule,
+    FontAwesomeModule,  
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
